356ea8bd04f13be753ea9f795043420af02716ca
harsha meher <113636677+hrshmeher@users.noreply.github.com>
2023-09-04 20:01:13 +0530

    limit-service "section 5"

---
+++ limits-service/src/main/java/com/learning/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,15 @@
+package com.learning.microservices.limitsservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+		
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+		return new Limits(1,1000);
+		
+	}
+
+}
